0c4982946393c6bf2bf87f639e5a3bcd8204c3dc
noobcodervp <vaibhavpatel02892@gmail.com>
2025-10-09 09:09:56 +0530

    Add change in diwali symbols and in room API

---
+++ utils/test.js
@@ -0,0 +1,99 @@
+export const diwaliSymbols = {
+    DY: "Diya", // Light of hope
+    FC: "Firecracker", // Burst of joy
+    SP: "Sparklers", // Twinkling excitement
+    LP: "Lamp", // Bringer of light
+    LT: "Light", // Symbol of positivity
+    RG: "Rangoli", // Colorful welcome
+    CL: "Color", // Life’s palette
+    DR: "Decoration", // Festive makeover
+    GL: "Gold", // Shining prosperity
+    SR: "SiyaRam", // Devotion & unity
+    CB: "Celebration", // Togetherness
+    CD: "Candle", // Gentle glow
+    NC: "New Clothes", // Fresh beginnings
+    GR: "Garland", // Floral joy
+    PT: "Pot", // Traditional vessel
+    DN: "Dhanteras", // Wealthy start
+    KS: "Kesar", // Saffron grace
+    GH: "Ghee", // Pure essence
+    ML: "Milk", // Nourishment
+    LD: "Laddoo", // Sweet victory
+    BR: "Barfi", // Soft delight
+    HL: "Halwa", // Comfort food
+    RS: "Rasmalai", // Sweet indulgence
+    SW: "Sweets", // Happiness bites
+    CH: "Chocolates", // Modern mithai
+    PK: "Pakkwan", // Festive platter
+    FT: "Festival", // Grand vibe
+    FM: "Family", // Heart of Diwali
+    FR: "Friends", // Endless laughter
+    HM: "Harmony", // Perfect balance
+    HP: "Happiness", // Cheer unlocked
+    PR: "Prosperity", // Growth & gain
+    WL: "Wealth", // Fortune symbol
+    SC: "Success", // Victory spirit
+    GF: "Gifts", // Token of love
+    BX: "Box", // Surprise inside
+    BL: "Blessing", // Divine favor
+    WF: "Welfare", // Well-being
+    RD: "Radiance", // Glow within
+    TK: "Tikka", // Sacred mark
+    CU: "Coconut", // Auspicious start
+    HS: "House", // Warm home
+    PD: "Pandal", // Community joy
+    RO: "Roshni", // Light personified
+    ST: "Star", // Night’s sparkle
+    DL: "Dreamlight", // Magical glow
+    AM: "Amavasya", // New moon night
+    NT: "Night", // Calm & glitter
+    EV: "Evening", // Celebration hour
+    MS: "Music", // Beats of joy
+    DC: "Dance", // Groove time
+    MG: "Magic", // Festive charm
+    MO: "Moment", // Cherished memory
+    TD: "Tradition", // Rooted pride
+    HC: "Home Cleaning", // Purity before festivity
+    CK: "Cooking", // Kitchen chaos
+    FD: "Food", // Feast mode
+    SK: "Sanskar", // Culture & respect
+    BV: "Belief", // Faith & trust
+    BG: "Bhog", // Devotional offering
+    CS: "Celebration Spirit", // Festive energy
+    HD: "Happy Diwali", // The main wish!
+    NY: "New Year", // Fresh start
+    BN: "Banner", // Welcome decor
+    CR: "Craftwork", // Handmade love
+    EL: "Envelope", // Cash carrier 😄
+    FG: "Fragrance", // Aroma of joy
+    HR: "Hues", // Shades of life
+    AR: "Art", // Creative touch
+    NB: "Notebook", // New goals
+    PF: "Photo Frame", // Captured memories
+    RL: "Ribbon Light", // Stream of color
+    VB: "Vibrance", // Lively energy
+    CI: "Chakri", // Spinning spark
+    RB: "Rassi Bomb", // Loud laugh
+    RC: "Rocket", // Sky high dream
+    BT: "Batti", // Light / gossip spark
+    PA: "Pataka", // Firecracker / hotshot
+    OM: "Om", // Sacred sound / OMG vibe
+    LA: "Lakshmi", // Wealth & grace
+    MT: "Mithai", // Sweet talker
+    DJ: "DJ Night", // Music & drama
+    TP: "Toran", // Entry decor / time pass
+    DP: "Diya Plate", // Light tray / display pic
+    SI: "Surprise", // Twist in the game
+    BB: "Big Blast", // Cracker / joke bomb
+    GP: "Gold Pot", // Rich & funny
+    BF: "Best Friend", // Partner in chaos
+    BP: "Bhai Photo", // Meme lord
+    CC: "Cash Cover", // Money envelope
+    KP: "Kaju Katli", // Smooth operator
+    ID: "Idol", // God / selfie pose
+    MB: "Matka Bomb", // Explosive mood
+};
+
+// verify if diwaliSymbols has 90 unique keys
+const keys = Object.keys(diwaliSymbols);
+console.log(keys.length);
